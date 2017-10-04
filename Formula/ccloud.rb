353a1d9387f19abf2b29e7f349c7c123e732de6d
Travis Thompson <trthomps@confluent.io>
2017-10-04 10:59:59 -0700

    Bump to 0.1.8

--- Formula/ccloud.rb
+++ Formula/ccloud.rb
@@ -1,8 +1,8 @@
 class Ccloud < Formula
   desc 'Confluent Cloud CLI tool'
   homepage 'https://www.confluent.io/confluent-cloud/'
-  url 'https://s3-us-west-2.amazonaws.com/confluent.cloud/cli/ccloud-0.1.6.tar.gz'
-  sha256 'bd62316c037d837895e748e562967f5f3e0242caa9dfb3d3466f03afa655ddb2'
+  url 'https://s3-us-west-2.amazonaws.com/confluent.cloud/cli/ccloud-0.1.8.tar.gz'
+  sha256 'b1d8032fc495ca6efb78cfa80d13d9edadc0baa061cf51510b55e621dce70f13'
 
   depends_on :java
 
