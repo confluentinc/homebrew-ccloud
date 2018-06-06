9d8baedb1afda9178e6a25913f9e370f635b8490
Cody A. Ray <cody@confluent.io>
2018-06-06 10:28:00 -0700

    Brew formula update for confluent-cli version v0.6.0

--- Formula/confluent-cli.rb
+++ Formula/confluent-cli.rb
@@ -1,9 +1,9 @@
 class ConfluentCli < Formula
   desc "Confluent CLI"
   homepage "https://confluent.io"
-  url "https://github.com///releases/download/v0.6.0/confluent-cli_0.6.0_Darwin_x86_64.tar.gz"
+  url "https://github.com/confluentinc/cli/releases/download/v0.6.0/confluent-cli_0.6.0_Darwin_x86_64.tar.gz"
   version "0.6.0"
-  sha256 "a066be0b5ecb91a145fb86a7750a5191e9f021d69401a6baacffd8a48726b3b2"
+  sha256 "0afb0fd4b55652029a8148437dc85f9b53d002034a22b7863c9c89685ed7524d"
 
   def install
     bin.install "confluent"
